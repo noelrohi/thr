ed42750c7a6ae034499d4d6693e601751c3d3a24
gneiru <noelrohi59@gmail.com>
2024-04-09 21:45:38 +0800

    feat: Custom drizzle adapter

--- src/auth/config.ts
+++ src/auth/config.ts
@@ -1,15 +1,16 @@
+import { pgDrizzleAdapter as DrizzleAdapter } from "@/auth/adapter";
 import type { DefaultSession } from "@auth/core/types";
-import { DrizzleAdapter } from "@auth/drizzle-adapter";
 import type { NextAuthConfig } from "next-auth";
-
 import { db, tableCreator } from "@/db";
-
 import { env } from "@/env";
+import type { Adapter } from "@auth/core/adapters";
 
 declare module "next-auth" {
   interface Session {
     user: {
       id: string;
+      username: string;
+      bio: string;
     } & DefaultSession["user"];
   }
 }
@@ -19,7 +20,7 @@ export const authConfig = {
   pages: {
     signIn: "/signin",
   },
-  adapter: DrizzleAdapter(db, tableCreator),
+  adapter: DrizzleAdapter(db, tableCreator) as unknown as Adapter,
   providers: [],
   secret: env.NEXTAUTH_SECRET,
   callbacks: {
@@ -27,6 +28,10 @@ export const authConfig = {
       ...session,
       user: {
         ...session.user,
+        // @ts-expect-error
+        username: user.username,
+        // @ts-expect-error
+        bio: user.bio,
         id: user.id,
       },
     }),
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -62,3 +62,13 @@ export function toRelativeTime(
     .format(Math.floor(deltaSeconds / divisor), units[unitIndex])
     .replaceAll(" ago", "");
 }
+
+type NonNullableProps<T> = {
+  [P in keyof T]: null extends T[P] ? never : P;
+}[keyof T];
+
+export function stripUndefined<T>(obj: T): Pick<T, NonNullableProps<T>> {
+  const result = {} as T;
+  for (const key in obj) if (obj[key] !== undefined) result[key] = obj[key];
+  return result;
+}
